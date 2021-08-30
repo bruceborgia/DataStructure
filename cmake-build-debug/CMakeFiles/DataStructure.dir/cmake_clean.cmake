d51bec4b3a6b0a98b597c4ff21a1d8082e74edbe
bruceborgia <bruceborgia@126.com>
2021-08-30 17:14:20 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/DataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure.dir/main.cpp.o"
+  "DataStructure"
+  "DataStructure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
