4f11f24c267e4060de571f434699a8fabf78bafc
bruceborgia <bruceborgia@126.com>
2021-08-31 08:50:35 +0800

    test

--- SqList.cpp
+++ SqList.cpp
@@ -1,7 +1,14 @@
 #include "iostream"
 using namespace std;
 
+typedef struct LNode
+{
+    int data;
+    struct LNode *next;
+}LNode;
+
 int main(){
     cout<<"hello world";
+    cout<<"jj";
     return 0;
 }
